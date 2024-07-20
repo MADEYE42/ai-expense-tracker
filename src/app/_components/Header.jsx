4da8dea695dbb00e032fa329e7f36f94196621b1
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-21 02:48:48 +0530

    nAVBAR

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,10 +1,29 @@
 "use client"
 import React from 'react';
 import{Button} from '@/components/ui/button'
-
+import { UserButton, useUser } from '@clerk/nextjs';
+import LOGO from '../../../public/logo.png'
+import Image from 'next/image';
+import Link from 'next/link';
 const Header = () => {
+   const {user,signedIn} = useUser();
   return (
-    <div>Button</div>
+    <div className='p-5 flex justify-between items-center border shadow-sm'>
+        <div className='flex flex-row items-center'>
+            <Image src={LOGO} alt='LOGO' width={40} height={25} className='rounded-full'/>
+            <span className='text-purple-800 font-bold text-xl'>MoneyGotNoChill</span>
+        </div>
+        {signedIn?(<UserButton/>):
+            <div className='flex gap-3 items-center'>
+                <Link href='/dashboard'>
+                    <Button variant="outline">Dashboard</Button>
+                </Link>
+                <Link href='/dashboard'>
+                    <Button className>Get Started</Button>
+                </Link>
+            </div>
+        }
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#543067",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
